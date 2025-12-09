12bc58ff4c3a695bf5e3edf4e9c6472f0c038fe9
Leonardo Ferreira <leonardof108@gmail.com>
2025-12-09 12:27:11 -0300

    Fixed missing a handler for MethodArgumentNotValidException, which is thrown when the @Valid annotation fails

--- src/main/java/com/leonardof108/TransactionStatsAPI/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/leonardof108/TransactionStatsAPI/api/exception/GlobalExceptionHandler.java
@@ -1,23 +1,23 @@
 package com.leonardof108.TransactionStatsAPI.api.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-// src/main/java/com/itau/desafio/api/exception/GlobalExceptionHandler.java
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // Handle manual logic exceptions (like Future dates)
-    @ExceptionHandler(UnprocessableEntityException.class)
-    public ResponseEntity<Void> handleUnprocessable() {
-        return ResponseEntity.unprocessableEntity().build();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public void handleValidationExceptions() {
     }
 
-    // Handle JSON parsing errors (Standard Spring 400)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Void> handleBadRequest() {
-        return ResponseEntity.badRequest().build();
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleHttpMessageNotReadable() {
     }
 }
