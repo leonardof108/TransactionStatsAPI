cfb58b307dd497de9129c22193873d85b0c24e54
Leonardo Ferreira <leonardof108@gmail.com>
2025-12-09 10:09:48 -0300

    Add Spring Initializr

---
+++ src/main/java/com/leonardof108/TransactionStatsAPI/TransactionStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.leonardof108.TransactionStatsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStatsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardof108/TransactionStatsAPI/TransactionStatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardof108.TransactionStatsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
