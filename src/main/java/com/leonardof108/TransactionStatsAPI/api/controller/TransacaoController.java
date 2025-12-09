a35ea4c14a978b7a27569ac7b97c26d76cc25e2d
Leonardo Ferreira <leonardof108@gmail.com>
2025-12-09 12:26:24 -0300

    Fixed missing the @Valid annotation on the criarTransacao method's request body

--- src/main/java/com/leonardof108/TransactionStatsAPI/api/controller/TransacaoController.java
+++ src/main/java/com/leonardof108/TransactionStatsAPI/api/controller/TransacaoController.java
@@ -2,6 +2,7 @@ package com.leonardof108.TransactionStatsAPI.api.controller;
 
 import com.leonardof108.TransactionStatsAPI.api.dto.TransacaoRequest;
 import com.leonardof108.TransactionStatsAPI.api.service.TransacaoService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class TransacaoController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> criarTransacao(@RequestBody TransacaoRequest request) {
+    public ResponseEntity<Void> criarTransacao(@Valid @RequestBody TransacaoRequest request) {
         // Logic validations are handled in the service
         // If JSON is invalid Spring throws 400 automatically
         service.adicionarTransacao(request);
