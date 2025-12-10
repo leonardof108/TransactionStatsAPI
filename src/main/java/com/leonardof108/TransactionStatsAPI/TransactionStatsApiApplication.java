642c068cb3cbebd759a6dcfe84baece7ee41c5f7
Leonardo Ferreira <leonardof108@gmail.com>
2025-12-10 10:13:55 -0300

    Enable the @Scheduled annotation

--- src/main/java/com/leonardof108/TransactionStatsAPI/TransactionStatsApiApplication.java
+++ src/main/java/com/leonardof108/TransactionStatsAPI/TransactionStatsApiApplication.java
@@ -2,8 +2,10 @@ package com.leonardof108.TransactionStatsAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TransactionStatsApiApplication {
 
 	public static void main(String[] args) {
